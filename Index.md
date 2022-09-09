9b1269439cfadb9cdbca2fdfff2aed0607b8705d
Tinmarino <tinmarino@gmail.com>
2022-09-08 21:22:10 -0300

    first commit

---
+++ Index.md
@@ -0,0 +1,89 @@
+---
+comment: https://stackexchange.com/sites
+wiki_css: ../Css/color_dark_solarized.css, ../Css/layout_toc.css
+header-includes: <script type="text/javascript" src="../Css/js_masonry_desandro.js"></script>
+---
+
+<br/> <br/> <br/> <br/> <br/>
+
+## <img src='../res/img/stackoverflow_white_logo.svg' style='max-width:45%; margin-top:-200px'>
+
+### Interpreted                        
+* [Bash](Bash_top_100)               
+* [Git](Git_top_100)                 
+* [Js](Js_top_100)                   
+* [Python](Python_top_100)           
+* [Py/Numpy](Numpy_top_100)          
+* [Perl](Perl_top_100)               
+* [Ruby](Ruby_top_100)               
+* [Vim](Vim_top_100)                 
+
+### Compiled
+* [C](C_top_100)
+* [C++](C++_top_100)
+* [Java](Java_top_100)
+
+### Markup
+* [Tex](Tex_top_100)
+* [Css](Css_top_100)
+* [Html](Html_top_100)
+* [Sql](Sql_top_100)
+
+### Computing
+* [Algorithm](Algorithm_top_100)
+* [Linux](Linux_top_100)
+* [Qt](Qt_top_100)
+* [Windows](Windows_top_100)
+* [Regex](Regex_top_100)
+
+
+
+
+<br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/>
+
+## <img src='../res/img/stackexchange_white_logo.svg' style='max-width:45%; margin-top:-200px; margin-bottom: -20px; margin-left: -40px; ' >
+
+### Computing
+
+* [Ai](Ai_top_100_se) 
+* [Data_science](Data_science_top_100_se) 
+* [Reverse_engineering](Reverse_engineering_top_100_se) 
+* [Computer_graphics](Computer_graphics_top_100_se) 
+* [Cryptography](Cryptography_top_100_se) 
+* [Computer_science](Computer_science_top_100_se) 
+* [Software_quality_assurance](Software_quality_assurance_top_100_se) 
+* [Software_engineering](Software_engineering_top_100_se) 
+* [Computer_science_theory](Computer_science_theory_top_100_se) 
+
+#### Computing again
+* [Unix](Unix_top_100_se) 
+* [Tex](Tex_top_100_se) 
+* [Vi](Vim_top_100_se) 
+
+
+### Science
+
+* [Physics](Physics_top_100_se) 
+* [Astronomy](Astronomy_top_100_se) 
+* [Engineering](Engineering_top_100_se) 
+* [Geology](Geology_top_100_se) 
+* [Chemistry](Chemistry_top_100_se) 
+* [Biology](Biology_top_100_se) 
+* [Geography](Geography_top_100_se) 
+* [Math](Math_top_100_se) 
+* [Electronics](Electronics_top_100_se) 
+
+### Interesting
+
+* [Dog](Dog_top_100_se) 
+* [Cat](Cat_top_100_se) 
+* [Philosophy](Philosophy_top_100_se) 
+* [Parenting](Parenting_top_100_se) 
+* [Mythology](Mythology_top_100_se) 
+* [Travel](Travel_top_100_se) 
+* [Puzzling](Puzzling_top_100_se) 
+* [Linguistics](Linguistics_top_100_se) 
+* [Fitness](Fitness_top_100_se) 
+
+
+
---
+++ wiki2html.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+#
+# shellcheck disable=SC2002  # Useless cat.
+
+# shellcheck disable=SC2034  # FORCE appears unused
+FORCE="$1"
+SYNTAX="$2"
+EXTENSION="$3"
+OUTPUTDIR="$4"
+INPUT="$5"
+CSSFILE="$6" # Not used bad concatenation
+
+FILE=$(basename "$INPUT")
+FILENAME=$(basename "$INPUT")
+FILENAME=${FILENAME%%.md}
+FILEPATH=${INPUT%"$FILE"}
+OUTDIR=${OUTPUTDIR%"$FILEPATH"*}
+OUTPUT="$OUTDIR"/$FILENAME
+
+
+# Define $MATH
+HAS_MATH=$(grep -o "\$\$.\+\$\$" "$INPUT")
+if [ -n "$HAS_MATH" ]; then
+  MATH="--mathjax=https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+else
+  MATH=""
+fi
+
+
+# Compile for unix (with pandoc & Perl)
+# TODO if there is no level2 afeter level1 ?
+munix(){
+  [[ "$INPUT" =~ '/diary/' ]] && return
+  # Read metadata
+  meta=$(perl -0777 -ne '$_ =~ /^ *---(.+?)---/s ; $res .= $1; END { print $res; }' "$INPUT");
+
+  # Read `wiki_css:` in metadata
+  CSSFILE=$(realpath --relative-to="$OUTDIR" "$HOME/wiki/wiki_html/Css/include.css")
+  export CSS_EMBED=$(echo -e "$meta" | perl -0777 -ne 'while ($_ =~ /^wiki_css:(.+)$/mg) {$res .= " -c " . ($1 =~ s/,/ -c /gr)}; print substr $res, 4')
+  [ "$CSS_EMBED" ] && export CSSFILE="$CSS_EMBED"
+
+  # Read `wiki_pandoc:` in metadata
+  export PANDOC=$(echo -e "$meta" | perl -0777 -ne 'while ($_ =~ /^wiki_pandoc:(.+)$/mg) {print $1}')
+
+  # Convertion pipeline
+  cat "$INPUT" |
+  # Add h3-section betewwen h3/h2 headings and end
+  #
+  # Newline after title
+  perl -pe ' s/(\s*)(#.*)/\n\2/;' |
+  # Replace vim by language-vim for prism color higlight
+  perl -pe ' s/```vim/```language-vim/;' |
+  # Change links: add html
+  perl -pe  ' s/(\[.+?\])\(([^#)]+?)\)/\1(\2.html)/g;' |
+  # Double the new line before code
+  perl -0pe ' s/((^|\n\S)[^\n]*)\n\t([^*])/\1\n\n\t\3/g;' |
+  # Remove spaces in void lines
+  perl -lpe ' s/^\s*$//;' |
+  # Debug
+  # tee test.md |
+  # Compile: can add  --standalone --self-contained and --include-header=<file>
+  pandoc \
+    $MATH $PANDOC \
+    --highlight-style breezedark \
+    --section-divs \
+    -f "$SYNTAX" \
+    -t html \
+    -T "$FILE" \
+    -c "$CSSFILE" \
+    > "$OUTPUT.html"
+  echo -e "Css: $CSSFILE \nPandoc: $PANDOC \nMeta: $meta"
+}
+
+
+# Obsolete ?
+mtermux(){
+  cat "$INPUT" |
+  perl -pe  ' s/(\[.+\])\(([^#)]+)\)/\1(\2.html)/g' |
+	python -m markdown |
+	sed -r 's/<li>(.*)\[ \]/<li class="todo done0">\1/g; s/<li>(.*)\[X\]/<li class="todo done4">\1/g' >"$OUTPUT.html"
+	# pandoc $MATH -s -f $SYNTAX -t html -c $CSSFILENAME | \
+}
+
+
+# Main
+mmain(){
+  tmp=$(uname -a)
+  
+  shopt -s nocasematch
+  if [[ "$tmp" =~ "android" ]] ; then
+  	mtermux
+  else
+  	munix
+  fi
+}
+
+
+# Start
+mmain
